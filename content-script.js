74baa291674c33a83908715b2c96e34570fe2d4b
brekol <brekol@gmail.com>
2017-01-05 00:02:22 +0100

    blur focus

--- content-script.js
+++ content-script.js
@@ -4,26 +4,36 @@ function log(text){
 }
 
 var textFieldValues = ["AAAaa", "aaaa","11111","10", "96080701155","a@a.pl", "07271214101","67020200603","AJM977446","BX5930092","6636965944","PL47035097377694",
-"PL251090957365511087681086789746","251090957365511087681086789746", "AA AA", "123456789","12-555", "77888","478585036", "87996238542736", "8777862177","aa"
+"PL251090957365511087681086789746","251090957365511087681086789746", "AA AA", "123456789","12-555", "77888","478585036", "87996238542736", "8777862177","aa","96080701155"
 ];
 
 var invokeEvent = function(element, eventType) {
+	$(element).focus();
     var changeEvent = document.createEvent("HTMLEvents");
     changeEvent.initEvent(eventType, true, true);
     element.dispatchEvent(changeEvent);
+    $(element).blur();
+    $(element).focus();
+    $(element).blur();
+}
+
+function validToFill(textField){
+	return !$(textField).next().hasClass('iew-CorrectIcon');
 }
 
 
 function fillValueForTextfield(textField, textFieldValues, valueCounter) {
 
-	if(valueCounter < textFieldValues.length && (!textField.value || $(textField).hasClass('iew-Error'))){
+	if(valueCounter < textFieldValues.length && validToFill(textField)){
 		$(textField).val(textFieldValues[valueCounter]);
     	invokeEvent(textField,"change");
     	setTimeout(function(){
-    	    if($(textField).hasClass('iew-Error')){
+    	    var hasClass = $(textField).hasClass('iew-Error');
+    	    console.log('hasClass: ' + hasClass)
+    	    if(hasClass){
     	        fillValueForTextfield(textField, textFieldValues, valueCounter+1);
     	    }
-    	},100);
+    	},200);
 	}
 
 }
