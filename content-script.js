38221f6a86823eb9fe6489ff7d2caeb177672171
brekol <brekol@gmail.com>
2017-01-05 20:31:03 +0100

    predefinied values

--- content-script.js
+++ content-script.js
@@ -3,9 +3,44 @@ function log(text){
 	console.log(text);
 }
 
-var textFieldValues = ["AAAaa", "aaaa","11111","10", "96080701155","a@a.pl", "07271214101","67020200603","AJM977446","BX5930092","6636965944","PL47035097377694",
-"PL251090957365511087681086789746","251090957365511087681086789746", "AA AA", "123456789","12-555", "77888","478585036", "87996238542736", "8777862177","aa","96080701155"
-];
+
+var midMap =
+	[
+		{
+			"names": ["pesel"],
+			"values": ["96080701155","478585036"]
+		},
+		{
+	        "names": ["imie","name"],
+	        "values": ["Jan","Kuba","Jacek"]
+	    },
+	    {
+	        "names": ["seriainumer","numerdowodu","numer_dowodu", "dowod", "idcard"],
+	        "values": ["ABI410622","AGM145394","AKW266309"]
+	    },
+	    {
+	        "names": ["rachunek","numerrachunku","numer_rachunku","account"],
+	        "values": ["PL97109003813413","RO17409318005209","RO522225089696946566608356730556","3696816720","PL24708197578827"]
+	    },
+	    {
+	        "names": ["regon"],
+	        "values": ["698359625","09207986618702","930844867"]
+	    },
+        {
+            "names": ["phone","telephone","telefon"],
+            "values": ["+48","123-456-789","123456789"]
+        },
+		{
+		 "names": ["mail"],
+		 "values": ["a@a.pl"]
+		},
+        {
+         "names": ["kodpocztowy","postal","kod_pocztowy"],
+         "values": ["78-888","88123","11-222"]
+        }
+	];
+
+var randomValues = ["aaa","123","a@a.pl"];
 
 var invokeEvent = function(element, eventType) {
 	$(element).focus();
@@ -28,9 +63,7 @@ function fillValueForTextfield(textField, textFieldValues, valueCounter) {
 		$(textField).val(textFieldValues[valueCounter]);
     	invokeEvent(textField,"change");
     	setTimeout(function(){
-    	    var hasClass = $(textField).hasClass('iew-Error');
-    	    console.log('hasClass: ' + hasClass)
-    	    if(hasClass){
+    	    if($(textField).hasClass('iew-Error')){
     	        fillValueForTextfield(textField, textFieldValues, valueCounter+1);
     	    }
     	},200);
@@ -38,22 +71,31 @@ function fillValueForTextfield(textField, textFieldValues, valueCounter) {
 
 }
 
+function filledWithPredefinedValues(textField, mid){
+	for(var index in midMap){
+		for (var j= 0;  j< midMap[index].names.length ; j++) {
+			var possibleMidMatch = midMap[index].names[j]
+			if(mid.indexOf(possibleMidMatch) != -1 ){
+				fillValueForTextfield(textField,midMap[index].values, 0);
+				return true;
+			}
+		}
+	}
+	return false;
+
+}
+
 function fillVisibleTextfields(){
 	var textFields = $('.iew-PageProxyPanelShown input:enabled.gwt-TextBox.iew-TextField');
 	log('Found ' + textFields.length + ' visible and enabled textfields.')
-	var valueCounter = 0;
 	for (var i = 0, len = textFields.length; i < len; i++) {
-		fillValueForTextfield(textFields[i],textFieldValues, valueCounter)
+		var textField = textFields[i];
+		var mid = $(textField).attr('mid').toLowerCase();
+		if(!filledWithPredefinedValues(textField,mid)){
+			fillValueForTextfield(textField,randomValues, 0);
+		}
 	}
 }
 
-function fillVisibleComboboxes(){
-	var textFields = $('.iew-PageProxyPanelShown input:enabled.gwt-TextBox.iew-TextField');
-	log('Found ' + textFields.length + ' visible and enabled textfields.')
-	var valueCounter = 0;
-	for (var i = 0, len = textFields.length; i < len; i++) {
-		fillValueForTextfield(textFields[i],textFieldValues, valueCounter)
-	}
-}
 
 fillVisibleTextfields();
