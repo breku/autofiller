b861aff3d20c66a2a78d4eb53932e934500bbbed
jbrek <jbrek@consdata.pl>
2017-07-06 11:19:06 +0200

    wspolwnioskodawca

--- content-script.js
+++ content-script.js
@@ -37,6 +37,16 @@ var widgets = [
 	{"name":"GesComplexComponent2.GesComplexComponent1.GesTextField8", "value":"AEI670249"},
 	{"name":"GesComplexComponent2.GesComplexComponent1.GesDatePicker1", "value":"1506031200000"},
 
+	{"name":"GesComplexComponent2.GesComplexComponent2.GesTextField1", "value":"Kuba A"},
+	{"name":"GesComplexComponent2.GesComplexComponent2.GesTextField2", "value":"Nazwisko A"},
+	{"name":"GesComplexComponent2.GesComplexComponent2.GesTextField3", "value":"Nazwisko Rodowe A"},
+	{"name":"GesComplexComponent2.GesComplexComponent2.GesTextField5", "value":"Imie Matki A"},
+	{"name":"GesComplexComponent2.GesComplexComponent2.GesTextField4", "value":"Nazwisko panieńskie matki A"},
+	{"name":"GesComplexComponent2.GesComplexComponent2.GesTextField6", "value":"08240603926"},
+	{"name":"GesComplexComponent2.GesComplexComponent2.GesTextField7", "value":"Miejsce urodzenia A"},
+	{"name":"GesComplexComponent2.GesComplexComponent2.GesTextField8", "value":"ARH366027"},
+	{"name":"GesComplexComponent2.GesComplexComponent2.GesDatePicker1", "value":"1506031200000"},
+
 	{"name":"GesComplexComponent3.GesComplexComponent1.GesTextField1", "value":"+48 123 123 123"},
 	{"name":"GesComplexComponent3.GesComplexComponent1.GesTextField2", "value":"a@a.pl"},
 	{"name":"GesComplexComponent3.GesComplexComponent1.GesComplexComponent1.GesCombobox1", "value":"AL"},
@@ -46,6 +56,16 @@ var widgets = [
 	{"name":"GesComplexComponent3.GesComplexComponent1.GesComplexComponent1.GesTextField4", "value":"23-423"},
 	{"name":"GesComplexComponent3.GesComplexComponent1.GesComplexComponent1.GesTextField5", "value":"Miejscowość"},
 
+	{"name":"GesComplexComponent3.GesComplexComponent2.GesTextField1", "value":"+48 999 999 999"},
+	{"name":"GesComplexComponent3.GesComplexComponent2.GesTextField2", "value":"b@b.pl"},
+	{"name":"GesComplexComponent3.GesComplexComponent2.GesComplexComponent3.GesCombobox1", "value":"PL"},
+	{"name":"GesComplexComponent3.GesComplexComponent2.GesComplexComponent3.GesTextField1", "value":"Ulica2"},
+	{"name":"GesComplexComponent3.GesComplexComponent2.GesComplexComponent3.GesTextField2", "value":"9"},
+	{"name":"GesComplexComponent3.GesComplexComponent2.GesComplexComponent3.GesTextField3", "value":"10"},
+	{"name":"GesComplexComponent3.GesComplexComponent2.GesComplexComponent3.GesTextField4", "value":"88-999"},
+	{"name":"GesComplexComponent3.GesComplexComponent2.GesComplexComponent3.GesTextField5", "value":"Miejscowość WIELKA"},
+
+
 	{"name":"GesComplexComponent4.GesComplexComponent1.GesTileGroup1", "value":"2"},
 	{"name":"GesComplexComponent4.GesComplexComponent1.GesTileGroup3", "value":"2"},
 
@@ -54,6 +74,9 @@ var widgets = [
 
 	{"name":"GesComplexComponent4.GesComplexComponent3.GesTileGroup1", "value":"2"},
 	{"name":"GesComplexComponent4.GesComplexComponent3.GesTileGroup2", "value":"2"},
+
+	{"name":"GesComplexComponent4.GesComplexComponent4.GesTileGroup1", "value":"2"},
+	{"name":"GesComplexComponent4.GesComplexComponent4.GesTileGroup2", "value":"2"},
 
 
 
