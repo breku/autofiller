174458422ea870275c7ff8cdbb6fb8878cf0340b
jbrek <jbrek@consdata.pl>
2018-02-23 15:26:37 +0100

    fix

--- src/js/autofiller.js
+++ src/js/autofiller.js
@@ -8,24 +8,27 @@ class Autofiller {
         });
     }
 
+    getWidget(element) {
+        var className = '.row-with-' + element["name"].replace(/\./g, "_");
+        return $(className);
+    }
+
+    createJsonObject(stkn, element) {
+        var jsonObject = {};
+        jsonObject["formNumber"] = stkn;
+        jsonObject["widgetChangedModel"] = {"id": element["name"], "changed": {"value": element["value"]}};
+        return jsonObject;
+    }
 
     fillForm(stkn, form) {
         console.log('Filling form=' + form["formName"]);
-        var jsonObject = {};
-        jsonObject["formNumber"] = stkn;
-
-        var that = this;
-        $.each(form.widgets, function (i, element) {
-            jsonObject["widgetChangedModel"] = {"id": element["name"], "changed": {"value": element["value"]}};
-
-            var className = '.row-with-' + element["name"].replace(/\./g, "_");
-
-
-            var widget = $(className);
-            if (widget.length != 0) {
-                that.callComponentUpdate(jsonObject);
+        for (let element of form.widgets) {
+            var widget = this.getWidget(element);
+            if (widget.length !== 0) {
+                var jsonObject = this.createJsonObject(stkn, element);
+                this.callComponentUpdate(jsonObject);
             }
-        })
+        }
 
 
     }
