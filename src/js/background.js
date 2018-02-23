2653bdf44e8397145dd4cb94213e93a310a15216
jbrek <jbrek@consdata.pl>
2018-02-23 15:20:16 +0100

    Fix

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,6 @@
 chrome.commands.onCommand.addListener(function(command){
         console.log(command);
-    	chrome.tabs.executeScript(null, { file: "jquery-3.1.1.min.js" , allFrames: true });
-    	chrome.tabs.executeScript(null, { file: "typed.min.js" });
+    	chrome.tabs.executeScript(null, { file: "lib/jquery-3.1.1.min.js" , allFrames: true });
         chrome.tabs.executeScript(null, { file: "browser-util.js" });
         chrome.tabs.executeScript(null, { file: "forms.js" });
         chrome.tabs.executeScript(null, { file: "content-script.js" });
