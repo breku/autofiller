c03e7790a76bf0ec8a6b64baf077584c2690f55b
jbrek <jbrek@consdata.pl>
2018-02-27 10:51:02 +0100

    fix

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ function click(e) {
     chrome.tabs.executeScript(null, { file: "src/js/browser-util.js" });
     chrome.tabs.executeScript(null, { file: "src/js/forms.js" });
     chrome.tabs.executeScript(null, { file: "src/js/autofiller.js" });
+    chrome.tabs.executeScript(null, {file: "src/js/rest-client.js"});
     chrome.tabs.executeScript(null, { file: "src/js/main.js" });
     chrome.tabs.executeScript(null, { file: "src/js/entrypoint.js" });
 
